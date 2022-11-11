3e487bc8513f12a7380ebb64594f9595dbd6ed5a
Tommy Bellaire <rtb+cs1300@brown.edu>
2022-11-10 20:30:07 -0500

    push

--- src/App.js
+++ src/App.js
@@ -11,17 +11,19 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [cartItems, setCartItems] = useState([])
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div className="bakery-container">
+        {bakeryData.map((item, index) => ( 
+        <BakeryItem item ={item}/>
+        ))}
+      </div>
 
       <div>
         <h2>Cart</h2>
+        {cartItems.map((item, index) => <p> {item.name} </p>)}
         {/* TODO: render a list of items in the cart */}
       </div>
     </div>
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,14 @@
 // TODO: create a component that displays a single bakery item
+function BakeryItem(props) {
+    return (
+        <div className="BakeryItem">
+            <h1>Name: {props.item.name}</h1>
+            <h2>Price: {props.item.price}</h2>
+            <p> Description: {props.item.description}</p>
+            <img src={props.item.image}/>
+        </div>
+    )
+
+}
+
+export default BakeryItem;
